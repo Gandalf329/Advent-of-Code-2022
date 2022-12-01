f49a7fca2608fc66612aac3229377d38c8493dc7
Никита Похлебаев <pohlebaev.n@yandex.ru>
2022-12-01 21:08:31 +0300

    Add day 1 code

---
+++ Days/01_Calorie Counting.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Advent_of_Code_2022.Days;
+public class _01_Calorie_Counting
+{
+    string path = "D:\\progVis\\Advent-of-Code-2022\\input\\1day_input.txt";
+    List<int> data = new List<int>();
+    public void GetData()
+    {
+        int sum = 0;
+        using (StreamReader fs = new StreamReader(path))
+        {
+            while (true)
+            {
+                string temp = fs.ReadLine();
+                if (temp == null)
+                {
+                    data.Add(sum);
+                    break;
+                }
+
+                if (temp.Length == 0)
+                {
+                    data.Add(sum);
+                    sum = 0;
+                }
+                else
+                {
+                    sum += int.Parse(temp);
+                }
+            }
+        }
+    }
+    public int Result1()
+    {
+        return data.Max();
+    }
+    public int Result2()
+    {
+        var data2 = from d in data orderby d descending select d;
+        return data2.Take(3).Sum();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Advent_of_Code_2022;
+﻿using Advent_of_Code_2022.Days;
+namespace Advent_of_Code_2022;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        var day1 = new _01_Calorie_Counting();
+        day1.GetData();       
+
         Console.WriteLine("Advent of code 2022!");
+        Console.WriteLine($"Result 1: {day1.Result1()} \t Result 2: {day1.Result2()}");
+        
     }
 }
