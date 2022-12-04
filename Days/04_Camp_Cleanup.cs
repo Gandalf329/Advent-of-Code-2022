1c504d1da053e9d618e1ad2b77abb62ae3223e7a
Никита Похлебаев <pohlebaev.n@yandex.ru>
2022-12-04 19:41:35 +0300

    Add 4 day code

---
+++ Days/04_Camp_Cleanup.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2022.Days;
+internal class _04_Camp_Cleanup
+{
+    string path = "D:\\progVis\\Advent-of-Code-2022\\input\\4day_input.txt";
+    List<string> data = new List<string>();
+
+    public void GetData()
+    {
+        using (StreamReader fs = new StreamReader(path))
+        {
+            while (true)
+            {
+                string temp = fs.ReadLine();
+                if (temp == null)
+                {
+                    break;
+                }
+                data.Add(temp);
+            }
+        }
+    }
+    public int Result1()
+    {
+        int result = 0;
+
+        foreach (string item in data)
+        {
+            var part = item.Split(",");
+            var numbers1 = part[0].Split('-');
+            var numbers2 = part[1].Split('-');
+            if (int.Parse(numbers1[0]) <= int.Parse(numbers2[0]) && int.Parse(numbers1[1]) >= int.Parse(numbers2[1]))
+            {
+                result++;
+            }else if(int.Parse(numbers1[0]) >= int.Parse(numbers2[0]) && int.Parse(numbers1[1]) <= int.Parse(numbers2[1]))
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+    public int Result2()
+    {
+        int result = 0;
+
+        foreach (string item in data)
+        {
+            var part = item.Split(",");
+            var numbers1 = part[0].Split('-');
+            var numbers2 = part[1].Split('-');
+            var min1 = int.Parse(numbers1[0]);
+            var min2 = int.Parse(numbers2[0]);
+            var max1 = int.Parse(numbers1[1]);
+            var max2 = int.Parse(numbers2[1]);
+
+            if (int.Parse(numbers1[0]) >= int.Parse(numbers2[0]) &&
+                int.Parse(numbers1[0]) <= int.Parse(numbers2[1]))
+            {
+                result++;
+                continue;
+            }
+
+            // numbers1 1 falls in between numbers2 0 and 1
+            if (int.Parse(numbers1[1]) >= int.Parse(numbers2[0]) &&
+                int.Parse(numbers1[1]) <= int.Parse(numbers2[1]))
+            {
+               result++;
+                continue;
+            }
+
+            // numbers2 0 falls in between numbers1 0 and 1
+            if (int.Parse(numbers2[0]) >= int.Parse(numbers1[0]) &&
+                int.Parse(numbers2[0]) <= int.Parse(numbers1[1]))
+            {
+                result++;
+                continue;
+            }
+
+            // numbers2 1 falls in between numbers1 0 and 1
+            if (int.Parse(numbers2[1]) >= int.Parse(numbers1[0]) &&
+                int.Parse(numbers2[1]) <= int.Parse(numbers1[1]))
+            {
+                result++;
+                continue;
+            }
+        }
+        return result;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,14 +8,17 @@ internal class Program
         var day1 = new _01_Calorie_Counting();
         var day2 = new _02_Rock_Paper_Scissors();
         var day3 = new _03_Rucksack_Reorganization();
-        
+        var day4 = new _04_Camp_Cleanup();
+
         day1.GetData();
         day2.GetData();
         day3.GetData();
+        day4.GetData();
 
         Console.WriteLine("Advent of code 2022!");
         Console.WriteLine($"Day 1. Result 1: {day1.Result1()} \t Result 2: {day1.Result2()}");
         Console.WriteLine($"Day 2. Result 1: {day2.Result1()} \t Result 2: {day2.Result2()}");
         Console.WriteLine($"Day 3. Result 1: {day3.Result1()} \t Result 2: {day3.Result2()}");
+        Console.WriteLine($"Day 4. Result 1: {day4.Result1()} \t Result 2: {day4.Result2()}");
     }
 }
