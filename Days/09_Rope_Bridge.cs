d9c62ce521ac85ecec9ae15a15022479e5a6ac01
Никита Похлебаев <pohlebaev.n@yandex.ru>
2022-12-09 17:14:15 +0300

    Add 9 day code (1 star)

---
+++ Days/09_Rope_Bridge.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2022.Days;
+internal class _09_Rope_Bridge
+{
+    string path = "D:\\progVis\\Advent-of-Code-2022\\input\\9day_input.txt";
+    List<string> data = new List<string>();
+    public void GetData()
+    {
+        using (StreamReader fs = new StreamReader(path))
+        {
+            while (true)
+            {
+                string temp = fs.ReadLine();
+                if (temp == null)
+                {
+                    break;
+                }
+                data.Add(temp);
+            }
+        }
+    }
+
+    public bool Check(int rowT, int colT, int rowH, int colH, int c)
+    {
+        for (int i = -1; i < c; i++)
+        {
+            for (int j = -1; j < c; j++)
+            {
+                if (rowT + i == rowH && colT + j == colH)//1,1
+                {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+    public bool CheckItem(List<List<int>> pos, List<int> check)
+    {
+        foreach (var item in pos)
+        {
+            if (item[0] == check[0] && item[1] == check[1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public int Result1()
+    {
+        //int result = 0;
+
+        List<int> head = new List<int>() { 0, 0 };//row col
+        List<int> tail = new List<int>() { 0, 0 };
+        List<List<int>> positions = new List<List<int>>();
+        List<List<int>> result = new List<List<int>>();
+        positions.Add(new List<int>() { tail[0], tail[1] });
+        foreach (string item in data)
+        {
+            var move = item.Split(" ");
+            int num = int.Parse(move[1]);
+
+            for (int i = 0; i < num; i++)
+            {
+                int lastCol = 0;
+                int lastRow = 0;
+                switch (move[0])
+                {
+                    case "R":
+                        lastCol = 0;
+                        lastCol += head[1];
+                        lastRow = 0;
+                        lastRow += head[0];
+                        head[1] += 1;
+                        if (Check(tail[0], tail[1], head[0], head[1], 2))
+                        {
+                            tail[0] = lastRow;
+                            tail[1] = lastCol;
+                            if (CheckItem(positions, tail))
+                            {
+                                positions.Add(new List<int>() { tail[0], tail[1] });
+                            }
+
+                        }
+                        break;
+                    case "L":
+                        lastCol = 0;
+                        lastCol += head[1];
+                        lastRow = 0;
+                        lastRow += head[0];
+                        head[1] -= 1;
+                        if (Check(tail[0], tail[1], head[0], head[1], 2))
+                        {
+                            tail[0] = lastRow;
+                            tail[1] = lastCol;
+                            if (CheckItem(positions, tail))
+                            {
+                                positions.Add(new List<int>() { tail[0], tail[1] });
+                            }
+                        }
+                        break;
+                    case "U":
+                        lastCol = 0;
+                        lastCol += head[1];
+                        lastRow = 0;
+                        lastRow += head[0];
+                        head[0] += 1;
+                        if (Check(tail[0], tail[1], head[0], head[1], 2))
+                        {
+                            tail[0] = lastRow;
+                            tail[1] = lastCol;
+                            if (CheckItem(positions, tail))
+                            {
+                                positions.Add(new List<int>() { tail[0], tail[1] });
+                            }
+                        }
+                        break;
+                    case "D":
+                        lastCol = 0;
+                        lastCol += head[1];
+                        lastRow = 0;
+                        lastRow += head[0];
+                        head[0] -= 1;
+                        if (Check(tail[0], tail[1], head[0], head[1], 2))
+                        {
+                            tail[0] = lastRow;
+                            tail[1] = lastCol;
+                            if (CheckItem(positions, tail))
+                            {
+                                positions.Add(new List<int>() { tail[0], tail[1] });
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        result = positions.Distinct().ToList();
+        return result.Count;
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ internal class Program
         var day6 = new _06_Tuning_Trouble();
         var day7 = new _07_No_Space_Left_On_Device();
         var day8 = new _08_Treetop_Tree_House();
+        var day9 = new _09_Rope_Bridge();
 
         day1.GetData();
         day2.GetData();
@@ -22,6 +23,7 @@ internal class Program
         day6.GetData();
         day7.GetData();
         day8.GetData();
+        day9.GetData();
 
         Console.WriteLine("Advent of code 2022!");
         Console.WriteLine($"Day 1. Result 1: {day1.Result1()} \t Result 2: {day1.Result2()}");
@@ -32,5 +34,6 @@ internal class Program
         Console.WriteLine($"Day 6. Result 1: {day6.Result1()} \t Result 2: {day6.Result2()}");
         Console.WriteLine($"Day 7. Result 1: {day7.Result1()} \t Result 2: {day7.Result2()}");
         Console.WriteLine($"Day 8. Result 1: {day8.Result1()} \t Result 2: {day8.Result2()}");
+        Console.WriteLine($"Day 9. Result 1: {day9.Result1()} \t Result 2: {day8.Result2()}");
     }
 }
